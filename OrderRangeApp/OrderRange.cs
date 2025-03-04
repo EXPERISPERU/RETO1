3f6be0f2d1c2a9450787fa4bf7a79cc17586b9e9
imb-bestrada <bstrada4@gmail.com>
2025-03-04 11:17:22 -0500

    Agregar archivos de proyecto.

---
+++ OrderRangeApp/OrderRange.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderRangeApp
+{
+    class OrderRange
+    {
+        public (List<int> evenNumbers, List<int> oddNumbers) Build(List<int> numbers)
+        {
+            var evenNumbers = numbers.Where(n => n % 2 == 0).OrderBy(n => n).ToList();
+            var oddNumbers = numbers.Where(n => n % 2 != 0).OrderBy(n => n).ToList();
+            return (evenNumbers, oddNumbers);
+        }
+
+    }
+}
---
+++ OrderRangeApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using OrderRangeApp;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 2, 1, 4, 5, 7, 9, 10 , 12 };
+
+        OrderRange orderRange = new OrderRange();
+        var (evenNumbers, oddNumbers) = orderRange.Build(numbers);
+
+        Console.WriteLine("Arreglo: [" + string.Join(", ", numbers) + "]");
+        Console.WriteLine("Pares: [" + string.Join(", ", evenNumbers) + "]");
+        Console.WriteLine("Impares: [" + string.Join(", ", oddNumbers) + "]");
+
+        Console.WriteLine("\nPresiona cualquier tecla para salir...");
+        Console.ReadLine();
+    }
+}
